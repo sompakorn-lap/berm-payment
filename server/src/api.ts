6cbb8c8771e973c0dfa3bf44d0999c3f73996004
sompakorn-lap <sompakorn.lap@student.mahidol.edu>
2025-02-04 22:25:06 +0700

    feat(server): apply payment to api

--- server/src/api.ts
+++ server/src/api.ts
@@ -1,8 +1,10 @@
 import Elysia from "elysia";
 import userApi from "./features/user/user.api";
+import paymentApi from "./libs/stripe/stripe";
 
 const api = new Elysia({ prefix: "/api" })
   .use(userApi)
+  .use(paymentApi)
 ;
 
 export default api;
